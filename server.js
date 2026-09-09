03f52b987c900b47f960e6bed63955dea7b5c522
Gar <johnandrewgarcia123@gmail.com>
2026-09-09 21:31:34 +0800

    read endpoint with error 404

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+const inMemoryData = [{
+  id: 1, title: "CREATE",done:false
+}
+,
+{
+  id:2, title: "POST", done: true
+}
+,
+{
+  id:3, title: "DELETE" , done: false
+}]
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -12,6 +24,23 @@ app.get("/", (req, res) => {
 
 app.get("/health",(req , res) => {
   res.json({"status" : "ok"});
+});
+
+app.get("/tasks", (req,res) =>{
+  res.json(inMemoryData);
+});
+
+app.get("/tasks/:id",(req,res)=>{
+  const {id} = req.params;
+
+  const task = inMemoryData.find(task => task.id == id);
+  if(!task){
+    return res.status(404).json({
+      "error": `Task ${id} not found` 
+    })
+  }
+
+  res.json(task);
 });
 
 app.listen(PORT , () =>{
